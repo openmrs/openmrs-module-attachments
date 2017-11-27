8e795d2e07fed5261f85bdf042e9ed82faee4529
Os Lynn <ott.sathngam@gmail.com>
2017-11-27 05:57:03 -0600

    Fixed ticket ATT-6
       Checks original image dimensions. If the with and hight are within
       the THUMBNAIL range, we just append "_smallfile" to the original file
       name and NOT generate addition thumbnail file.

--- api/src/main/java/org/openmrs/module/attachments/obs/AbstractAttachmentHandler.java
+++ api/src/main/java/org/openmrs/module/attachments/obs/AbstractAttachmentHandler.java
@@ -1,148 +1,210 @@
 package org.openmrs.module.attachments.obs;
 
+import java.io.File;
+import java.io.IOException;
+import net.coobird.thumbnailator.Thumbnails;
 import org.apache.commons.io.FilenameUtils;
 import org.apache.commons.lang.StringUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.openmrs.Obs;
+import org.openmrs.api.APIException;
 import org.openmrs.module.attachments.AttachmentsConstants;
+import static org.openmrs.module.attachments.obs.ImageAttachmentHandler.THUMBNAIL_MAX_HEIGHT;
+import static org.openmrs.module.attachments.obs.ImageAttachmentHandler.THUMBNAIL_MAX_WIDTH;
 import org.openmrs.obs.ComplexData;
 import org.openmrs.obs.ComplexObsHandler;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 
 /**
- * Double inheritance class. The actual implementation parent must be set through
- * {@link #setParentComplexObsHandler()}.
+ * Double inheritance class. The actual implementation parent must be set
+ * through {@link #setParentComplexObsHandler()}.
  */
 public abstract class AbstractAttachmentHandler implements ComplexObsHandler {
-	
-	protected final Log log = LogFactory.getLog(getClass());
-	
-	private ComplexObsHandler parent;
-	
-	@Autowired
-	@Qualifier(AttachmentsConstants.COMPONENT_COMPLEXDATA_HELPER)
-	private ComplexDataHelper complexDataHelper;
-	
-	public AbstractAttachmentHandler() {
-		super();
-		setParentComplexObsHandler();
-	}
-	
-	protected ComplexDataHelper getComplexDataHelper() {
-		return complexDataHelper;
-	}
-	
-	/*
+
+    public final static String NO_THUMBNAIL_SUFFIX = "___nothumb__";
+    public final static String THUMBNAIL_SUFFIX = "_thumb";
+
+    protected final Log log = LogFactory.getLog(getClass());
+
+    private ComplexObsHandler parent;
+
+    @Autowired
+    @Qualifier(AttachmentsConstants.COMPONENT_COMPLEXDATA_HELPER)
+    private ComplexDataHelper complexDataHelper;
+
+    public AbstractAttachmentHandler() {
+        super();
+        setParentComplexObsHandler();
+    }
+
+    protected ComplexDataHelper getComplexDataHelper() {
+        return complexDataHelper;
+    }
+
+    /*
 	* To set the "real" implementation parent.
-	*/
-	abstract protected void setParentComplexObsHandler();
-	
-	/*
+     */
+    abstract protected void setParentComplexObsHandler();
+
+    /*
 	* Complex data CRUD - Read
-	*/
-	abstract protected ComplexData readComplexData(Obs obs, ValueComplex valueComplex, String view);
-	
-	/*
+     */
+    abstract protected ComplexData readComplexData(Obs obs, ValueComplex valueComplex, String view);
+
+    /*
 	* Complex data CRUD - Delete
-	*/
-	abstract protected boolean deleteComplexData(Obs obs, AttachmentComplexData complexData);
-	
-	/*
+     */
+    abstract protected boolean deleteComplexData(Obs obs, AttachmentComplexData complexData);
+
+    /*
 	* Complex data CRUD - Save (Update)
-	*/
-	abstract protected ValueComplex saveComplexData(Obs obs, AttachmentComplexData complexData);
-	
-	protected void setParent(ComplexObsHandler complexObsHandler) {
-		this.parent = complexObsHandler;
-	}
-	
-	final protected ComplexObsHandler getParent() {
-		return parent;
-	}
-	
-	public static String buildThumbnailFileName(String fileName) {
-		return FilenameUtils.removeExtension(fileName) + "_thumb" + "." + FilenameUtils.getExtension(fileName);
-	}
-	
-	/**
-	 * @param complexData An obs's complex data.
-	 * @return null if this is not our implementation, the custom {@link DocumentComplexData_old}
-	 *         otherwise.
-	 */
-	public static AttachmentComplexData fetchAttachmentComplexData(ComplexData complexData) {
-		
-		if ((complexData instanceof AttachmentComplexData) == false) {
-			return null;
-		}
-		
-		AttachmentComplexData docData = (AttachmentComplexData) complexData;
-		String instructions = docData.getInstructions();
-		if (instructions.equals(ValueComplex.INSTRUCTIONS_NONE)) {
-			return null;
-		}
-		
-		return docData;
-	}
-	
-	/*
-	* Drifts to our own CRUD overloadable routine when it is our implementation.
-	*/
-	@Override
-	final public Obs getObs(Obs obs, String view) {
-		
-		ValueComplex valueComplex = new ValueComplex(obs.getValueComplex());
-		if (valueComplex.isOwnImplementation() == false) { // not our implementation
-			return getParent().getObs(obs, view);
-		}
-		
-		if (StringUtils.isEmpty(view)) {
-			view = AttachmentsConstants.ATT_VIEW_ORIGINAL;
-		}
-		
-		ComplexData docData = readComplexData(obs, valueComplex, view);
-		obs.setComplexData(docData);
-		return obs;
-	}
-	
-	/*
-	* Drifts to our own CRUD overloadable routine when it is our implementation.
-	*/
-	@Override
-	final public boolean purgeComplexData(Obs obs) {
-		
-		AttachmentComplexData complexData = fetchAttachmentComplexData(obs.getComplexData());
-		if (complexData == null) { // not our implementation
-			if (obs.getComplexData() == null) {
-				log.error("Complex data was null and hence was not purged for OBS_ID='" + obs.getObsId() + "'.");
-				return false;
-			} else {
-				return getParent().purgeComplexData(obs);
-			}
-		}
-		
-		return deleteComplexData(obs, complexData);
-	}
-	
-	/*
-	* Drifts to our own CRUD overloadable routine when it is our implementation.
-	*/
-	@Override
-	final public Obs saveObs(Obs obs) {
-		
-		AttachmentComplexData complexData = fetchAttachmentComplexData(obs.getComplexData());
-		if (complexData == null) { // not our implementation
-			if (obs.getComplexData() == null) {
-				log.error("Complex data was null and hence was not saved for OBS_ID='" + obs.getObsId() + "'.");
-				return obs;
-			} else {
-				return getParent().saveObs(obs);
-			}
-		}
-		
-		ValueComplex valueComplex = saveComplexData(obs, complexData);
-		obs.setValueComplex(valueComplex.getValueComplex());
-		return obs;
-	}
+     */
+    abstract protected ValueComplex saveComplexData(Obs obs, AttachmentComplexData complexData);
+
+    protected void setParent(ComplexObsHandler complexObsHandler) {
+        this.parent = complexObsHandler;
+    }
+
+    final protected ComplexObsHandler getParent() {
+        return parent;
+    }
+
+    /*
+	* Appends "___nothumb__" the file
+     */
+    protected static String buildNoThumbnailFileFileName(String fileName) {
+        if (StringUtils.endsWith(FilenameUtils.removeExtension(fileName), NO_THUMBNAIL_SUFFIX)) {
+            return fileName;
+        } else {
+            return FilenameUtils.removeExtension(fileName) + NO_THUMBNAIL_SUFFIX + "." + FilenameUtils.getExtension(fileName);
+        }
+    }
+
+    /*
+     * Appends "_thumb" the file
+     */
+    public static String buildThumbnailFileName(String fileName) {
+        return FilenameUtils.removeExtension(fileName) + "_thumb" + "." + FilenameUtils.getExtension(fileName);
+    }
+
+    /**
+     * <p>
+     * The saveThumbnailOrRename method checks image dimension to see if the
+     * image is small enough to be its own thumbnail. If so, it will rename the
+     * image file by appending the <b>NO_THUMBNAIL_SUFFIX</b> to the file.
+     * Otherewise, it will create a small thumbnail file alongside the original
+     * file to be used as thumbnail image.</p>
+     *
+     * @param savedFile original file pointer
+     * @param imageHight image hight
+     * @param imageWidth image width
+     * @return savedFileName new renamed file name or orignal file name
+     */
+    public static String saveThumbnailOrRename(File savedFile, int imageHeight, int imageWidth) {
+
+        String savedFileName = savedFile.getName();
+
+        // Check for small image file
+        if ((imageHeight <= THUMBNAIL_MAX_HEIGHT) && (imageWidth <= THUMBNAIL_MAX_WIDTH)) {
+            // Rename the file by append NO_THUMBNAIL_SUFFIX to the file.
+            // Therefore, we will know this is a small file and no need for thumnail.
+            String newSavedFileName = buildNoThumbnailFileFileName(savedFile.getAbsolutePath());
+            File newSavedFile = new File(newSavedFileName);
+            savedFile.renameTo(newSavedFile);
+            savedFileName = buildNoThumbnailFileFileName(savedFileName);
+        } else {
+            // Saving the thumbnail
+            File dir = savedFile.getParentFile();
+            String thumbnailFileName = buildThumbnailFileName(savedFileName);
+            try {
+                Thumbnails.of(savedFile.getAbsolutePath()).size(THUMBNAIL_MAX_HEIGHT, THUMBNAIL_MAX_WIDTH).toFile(new File(dir, thumbnailFileName));
+            } catch (IOException e) {
+                throw new APIException("A thumbnail file could not be saved for obs with", e);
+            }
+        }
+
+        return savedFileName;
+    }
+
+    /**
+     * @param complexData An obs's complex data.
+     * @return null if this is not our implementation, the custom
+     * {@link DocumentComplexData_old} otherwise.
+     */
+    public static AttachmentComplexData fetchAttachmentComplexData(ComplexData complexData) {
+
+        if ((complexData instanceof AttachmentComplexData) == false) {
+            return null;
+        }
+
+        AttachmentComplexData docData = (AttachmentComplexData) complexData;
+        String instructions = docData.getInstructions();
+        if (instructions.equals(ValueComplex.INSTRUCTIONS_NONE)) {
+            return null;
+        }
+
+        return docData;
+    }
+
+    /*
+     * Drifts to our own CRUD overloadable routine when it is our implementation.
+     */
+    @Override
+    final public Obs getObs(Obs obs, String view) {
+
+        ValueComplex valueComplex = new ValueComplex(obs.getValueComplex());
+        if (valueComplex.isOwnImplementation() == false) { // not our implementation
+            return getParent().getObs(obs, view);
+        }
+
+        if (StringUtils.isEmpty(view)) {
+            view = AttachmentsConstants.ATT_VIEW_ORIGINAL;
+        }
+
+        ComplexData docData = readComplexData(obs, valueComplex, view);
+        obs.setComplexData(docData);
+        return obs;
+    }
+
+    /*
+     * Drifts to our own CRUD overloadable routine when it is our implementation.
+     */
+    @Override
+    final public boolean purgeComplexData(Obs obs) {
+
+        AttachmentComplexData complexData = fetchAttachmentComplexData(obs.getComplexData());
+        if (complexData == null) { // not our implementation
+            if (obs.getComplexData() == null) {
+                log.error("Complex data was null and hence was not purged for OBS_ID='" + obs.getObsId() + "'.");
+                return false;
+            } else {
+                return getParent().purgeComplexData(obs);
+            }
+        }
+
+        return deleteComplexData(obs, complexData);
+    }
+
+    /*
+     * Drifts to our own CRUD overloadable routine when it is our implementation.
+     */
+    @Override
+    final public Obs saveObs(Obs obs) {
+
+        AttachmentComplexData complexData = fetchAttachmentComplexData(obs.getComplexData());
+        if (complexData == null) { // not our implementation
+            if (obs.getComplexData() == null) {
+                log.error("Complex data was null and hence was not saved for OBS_ID='" + obs.getObsId() + "'.");
+                return obs;
+            } else {
+                return getParent().saveObs(obs);
+            }
+        }
+
+        ValueComplex valueComplex = saveComplexData(obs, complexData);
+        obs.setValueComplex(valueComplex.getValueComplex());
+        return obs;
+    }
 }
--- api/src/test/java/org/openmrs/module/attachments/obs/ImageAttachmentHandlerIT.java
+++ api/src/test/java/org/openmrs/module/attachments/obs/ImageAttachmentHandlerIT.java
@@ -7,7 +7,8 @@ import java.io.File;
 import java.io.IOException;
 
 import javax.imageio.ImageIO;
-
+import org.apache.commons.io.FilenameUtils;
+import org.apache.commons.lang.StringUtils;
 import org.junit.Assert;
 import org.junit.Test;
 import org.openmrs.Obs;
@@ -80,16 +81,65 @@ public class ImageAttachmentHandlerIT extends BaseModuleContextSensitiveTest {
 		
 		// Setup
 		Obs obs = testHelper.saveSmallSizeImageAttachment();
-		
-		// TODO: complete the unit test
+        MockMultipartFile mpFile = testHelper.getLastSavedTestImageFile();
+        String originalFileName = mpFile.getOriginalFilename();
+        
+        // Verif the buildNotThumbnailFileFileName()
+        File noThumbnailFile = new File(testHelper.getComplexObsDir() + "/" + ImageAttachmentHandler.buildNoThumbnailFileFileName(originalFileName));
+        Assert.assertTrue(noThumbnailFile.exists());
+        // Not create new file name since it contained NO_THUMBNAIL_SUFFIX
+        noThumbnailFile = new File(testHelper.getComplexObsDir() + "/" + ImageAttachmentHandler.buildNoThumbnailFileFileName(noThumbnailFile.getName()));
+        Assert.assertTrue(noThumbnailFile.exists());        
+        
+        // Verif the thumbnail file was NOT created
+        File thumbnail = new File(testHelper.getComplexObsDir() + "/" + ImageAttachmentHandler.buildThumbnailFileName(originalFileName));
+        Assert.assertFalse(thumbnail.exists());
+
+        //Assert.assertThat(thumbnail.length(), lessThan(file.length()));
+        BufferedImage img = ImageIO.read(noThumbnailFile);
+        Assert.assertTrue(ImageAttachmentHandler.THUMBNAIL_MAX_HEIGHT >= Math.max(img.getHeight(), img.getWidth()));
+
+        // Check that the file name contains the no thumbnail suffix
+        Assert.assertTrue(StringUtils.endsWith(FilenameUtils.removeExtension(noThumbnailFile.getName()), ImageAttachmentHandler.NO_THUMBNAIL_SUFFIX));        
 	}
-	
+    
+    @Test
+    public void deleteComplexData_shouldDeleteNoThumbnailFromDisk() throws IOException {
+
+        // Setup
+		Obs obs = testHelper.saveSmallSizeImageAttachment();
+        MockMultipartFile mpFile = testHelper.getLastSavedTestImageFile();
+        String originalFileName = mpFile.getOriginalFilename();  
+
+        // Replay
+        obs = Context.getObsService().getComplexObs(obs.getId(), AttachmentsConstants.ATT_VIEW_CRUD);
+        Context.getObsService().purgeObs(obs);
+
+        // Verif
+        File noThumbnailFile = new File(testHelper.getComplexObsDir() + "/" + ImageAttachmentHandler.buildNoThumbnailFileFileName(originalFileName));
+        Assert.assertFalse(noThumbnailFile.exists());
+    }
+    
 	@Test
 	public void readComplexData_shouldAlwaysFetchOriginalImageWhenSmallImage() throws IOException {
 		
 		// Setup
 		Obs obs = testHelper.saveSmallSizeImageAttachment();
-		
-		// TODO: complete the unit test
+        MockMultipartFile mpFile = testHelper.getLastSavedTestImageFile();
+        String originalFileName = mpFile.getOriginalFilename();
+        File noThumbnailFile = new File(testHelper.getComplexObsDir() + "/" + ImageAttachmentHandler.buildNoThumbnailFileFileName(originalFileName));
+        Assert.assertTrue(noThumbnailFile.exists());
+        byte[] expectedBytes = new BaseComplexData(noThumbnailFile.getName(), ImageIO.read(noThumbnailFile)).asByteArray();        
+
+        // Replay
+        obs = Context.getObsService().getComplexObs(obs.getId(), AttachmentsConstants.ATT_VIEW_THUMBNAIL);
+
+        // Verif
+        Assert.assertArrayEquals(expectedBytes, BaseComplexData.getByteArray(obs.getComplexData()));
+        
+        obs = Context.getObsService().getComplexObs(obs.getId(), AttachmentsConstants.ATT_VIEW_ORIGINAL);
+        
+        // Verif
+        Assert.assertArrayEquals(expectedBytes, BaseComplexData.getByteArray(obs.getComplexData()));             
 	}
 }
