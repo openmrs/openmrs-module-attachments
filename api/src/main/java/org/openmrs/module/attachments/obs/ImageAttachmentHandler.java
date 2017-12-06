78af11a325aaf2bf539ff0f3fb6118be3b8465a8
Os Lynn <ott.sathngam@gmail.com>
2017-12-06 07:50:05 -0600

    ATT-6: Handling small thumbnail file.
        The original image dimensions should be checked
        before jumping into creating its thumbnail. If
        the image is small enough to be its own thumbnail
        already, then the same file should be used for both.

--- api/src/main/java/org/openmrs/module/attachments/obs/ImageAttachmentHandler.java
+++ api/src/main/java/org/openmrs/module/attachments/obs/ImageAttachmentHandler.java
@@ -2,7 +2,10 @@ package org.openmrs.module.attachments.obs;
 
 import java.io.File;
 import java.io.IOException;
-
+import java.awt.image.BufferedImage;
+import javax.imageio.ImageIO;
+import org.apache.commons.io.FilenameUtils;
+import org.apache.commons.lang.StringUtils;
 import org.openmrs.Obs;
 import org.openmrs.api.APIException;
 import org.openmrs.module.attachments.AttachmentsConstants;
@@ -10,81 +13,90 @@ import org.openmrs.obs.ComplexData;
 import org.openmrs.obs.handler.AbstractHandler;
 import org.openmrs.obs.handler.ImageHandler;
 
-import net.coobird.thumbnailator.Thumbnails;
-
 public class ImageAttachmentHandler extends AbstractAttachmentHandler {
-	
-	public final static int THUMBNAIL_MAX_HEIGHT = 200;
-	
-	public final static int THUMBNAIL_MAX_WIDTH = THUMBNAIL_MAX_HEIGHT;
-	
-	public ImageAttachmentHandler() {
-		super();
-	}
-	
-	@Override
-	protected void setParentComplexObsHandler() {
-		setParent(new ImageHandler());
-	}
-	
-	@Override
-	protected ComplexData readComplexData(Obs obs, ValueComplex valueComplex, String view) {
-		
-		String fileName = valueComplex.getFileName();
-		if (view.equals(AttachmentsConstants.ATT_VIEW_THUMBNAIL)) {
-			fileName = buildThumbnailFileName(fileName);
-		}
-		
-		// We invoke the parent to inherit from the file reading routines.
-		Obs tmpObs = new Obs();
-		tmpObs.setValueComplex(fileName); // Temp obs used as a safety
-		tmpObs = getParent().getObs(tmpObs, AttachmentsConstants.IMAGE_HANDLER_VIEW); // ImageHandler doesn't handle
-		                                                                              // several views
-		ComplexData complexData = tmpObs.getComplexData();
-		
-		// Then we build our own custom complex data
-		return getComplexDataHelper().build(valueComplex.getInstructions(), complexData.getTitle(), complexData.getData(),
-		    valueComplex.getMimeType()).asComplexData();
-	}
-	
-	@Override
-	protected boolean deleteComplexData(Obs obs, AttachmentComplexData complexData) {
-		
-		// We use a temp obs whose complex data points to the file names
-		String fileName = complexData.getTitle();
-		String thumbnailFileName = buildThumbnailFileName(fileName);
-		
-		Obs tmpObs = new Obs();
-		tmpObs.setValueComplex(thumbnailFileName);
-		boolean isThumbNailPurged = getParent().purgeComplexData(tmpObs);
-		tmpObs.setValueComplex(fileName);
-		boolean isImagePurged = getParent().purgeComplexData(tmpObs);
-		
-		return isThumbNailPurged && isImagePurged;
-	}
-	
-	@Override
-	protected ValueComplex saveComplexData(Obs obs, AttachmentComplexData complexData) {
-		
-		// We invoke the parent to inherit from the file saving routines.
-		obs = getParent().saveObs(obs);
-		
-		File savedFile = AbstractHandler.getComplexDataFile(obs);
-		String savedFileName = savedFile.getName();
-		
-		// Saving the thumbnail
-		File dir = savedFile.getParentFile();
-		String thumbnailFileName = buildThumbnailFileName(savedFileName);
-		try {
-			Thumbnails.of(savedFile.getAbsolutePath()).size(THUMBNAIL_MAX_HEIGHT, THUMBNAIL_MAX_WIDTH)
-			        .toFile(new File(dir, thumbnailFileName));
-		}
-		catch (IOException e) {
-			getParent().purgeComplexData(obs);
-			throw new APIException("A thumbnail file could not be saved for obs with" + "OBS_ID='" + obs.getObsId() + "', "
-			        + "FILE='" + complexData.getTitle() + "'.", e);
-		}
-		
-		return new ValueComplex(complexData.getInstructions(), complexData.getMimeType(), savedFileName);
-	}
+
+    public final static int THUMBNAIL_MAX_HEIGHT = 200;
+
+    public final static int THUMBNAIL_MAX_WIDTH = THUMBNAIL_MAX_HEIGHT;
+
+    public ImageAttachmentHandler() {
+        super();
+    }
+
+    @Override
+    protected void setParentComplexObsHandler() {
+        setParent(new ImageHandler());
+    }
+
+    @Override
+    protected ComplexData readComplexData(Obs obs, ValueComplex valueComplex, String view) {
+
+        String fileName = valueComplex.getFileName();
+        if (view.equals(AttachmentsConstants.ATT_VIEW_THUMBNAIL)
+                && !StringUtils.endsWith(FilenameUtils.removeExtension(fileName), NO_THUMBNAIL_SUFFIX)) {
+            fileName = buildThumbnailFileName(fileName);
+        }
+
+        // We invoke the parent to inherit from the file reading routines.
+        Obs tmpObs = new Obs();
+        tmpObs.setValueComplex(fileName); // Temp obs used as a safety
+        tmpObs = getParent().getObs(tmpObs, AttachmentsConstants.IMAGE_HANDLER_VIEW); // ImageHandler doesn't handle several views
+        ComplexData complexData = tmpObs.getComplexData();
+
+        // Then we build our own custom complex data
+        return getComplexDataHelper().build(valueComplex.getInstructions(), complexData.getTitle(), complexData.getData(),
+                valueComplex.getMimeType()).asComplexData();
+    }
+
+    @Override
+    protected boolean deleteComplexData(Obs obs, AttachmentComplexData complexData) {
+
+        // We use a temp obs whose complex data points to the file names
+        String fileName = complexData.getTitle();
+        boolean isThumbNailPurged = true;
+        Obs tmpObs = new Obs();
+
+        if (!StringUtils.endsWith(FilenameUtils.removeExtension(fileName), NO_THUMBNAIL_SUFFIX)) {
+            String thumbnailFileName = buildThumbnailFileName(fileName);
+            tmpObs.setValueComplex(thumbnailFileName);
+            isThumbNailPurged = getParent().purgeComplexData(tmpObs);
+        }
+
+        tmpObs.setValueComplex(fileName);
+        boolean isImagePurged = getParent().purgeComplexData(tmpObs);
+
+        return isThumbNailPurged && isImagePurged;
+    }
+
+    @Override
+    protected ValueComplex saveComplexData(Obs obs, AttachmentComplexData complexData) {
+        int imageHeight = Integer.MAX_VALUE;
+        int imageWidth = Integer.MAX_VALUE;
+        String savedFileName = null;
+
+        // We invoke the parent to inherit from the file saving routines.
+        obs = getParent().saveObs(obs);
+
+        File savedFile = AbstractHandler.getComplexDataFile(obs);
+
+        // Get image dimensions
+        try {
+            BufferedImage image = ImageIO.read(savedFile);
+            imageHeight = image.getHeight();
+            imageWidth = image.getWidth();
+        } catch (IOException e) {
+            log.warn("The dimensions of image 'savedFileName' could not be determined, continuing with generating thumbnail.");
+        }
+
+        try {
+            savedFileName = saveThumbnailOrRename(savedFile, imageHeight, imageWidth);
+        } catch (APIException e) {
+            getParent().purgeComplexData(obs);
+            throw new APIException("A thumbnail file could not be saved for obs with"
+                    + "OBS_ID='" + obs.getObsId() + "', "
+                    + "FILE='" + complexData.getTitle() + "'.", e);
+        }
+
+        return new ValueComplex(complexData.getInstructions(), complexData.getMimeType(), savedFileName);
+    }
 }
