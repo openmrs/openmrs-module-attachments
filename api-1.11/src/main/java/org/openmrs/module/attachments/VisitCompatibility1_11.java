c71670e8c7598f4d8925f80350e12dc527125875
mgoodrich <mark.goodrich@live.com>
2018-01-08 13:07:18 -0500

    ATT-20: Upgrading Attachments module to support Core 2.1.x

--- api-1.11/src/main/java/org/openmrs/module/attachments/VisitCompatibility1_11.java
+++ api-1.11/src/main/java/org/openmrs/module/attachments/VisitCompatibility1_11.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Component;
 import java.util.List;
 
 @Component(AttachmentsConstants.COMPONENT_VISIT_COMPATIBILITY)
-@OpenmrsProfile(openmrsPlatformVersion = "1.11.4 - 2.1.*")
+@OpenmrsProfile(openmrsPlatformVersion = "1.11.4 - 2.*")
 public class VisitCompatibility1_11 implements VisitCompatibility {
 	
 	@Override
