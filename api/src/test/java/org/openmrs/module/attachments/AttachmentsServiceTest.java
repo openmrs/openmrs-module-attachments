a864f549a9f268e842ee4bb7e296097b01cc63d8
dilekamadushan <dileka.14@cse.mrt.ac.lk>
2018-03-21 18:00:05 +0530

    ATT-24 REST end point to query for lists of attachments, modified code on first review

--- api/src/test/java/org/openmrs/module/attachments/AttachmentsServiceTest.java
+++ api/src/test/java/org/openmrs/module/attachments/AttachmentsServiceTest.java
@@ -1,15 +1,32 @@
 package org.openmrs.module.attachments;
 
 import org.junit.Test;
+
 import org.openmrs.Encounter;
 import org.openmrs.Patient;
 import org.openmrs.Visit;
 
+import org.openmrs.api.context.Context;
+import org.openmrs.module.attachments.obs.Attachment;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
 public class AttachmentsServiceTest {
 	
+	@Autowired
+	private AttachmentsServiceImpl as ;
+	
 	@Test
-	public void getAttachments_shouldReturnAttachments(){
-		//toDo
+	public void getAttachments_shouldReturnAttachments() {
+		
+		// Setup
+		Patient patient = Context.getPatientService().getPatient(2);
+		Visit activeVisit = Context.getVisitService().getActiveVisitsByPatient(patient).get(0);
+		Encounter encounter = Context.getEncounterService().getEncountersByPatient(patient).get(0);
+		
+		List<Attachment> attachmentList = as.getAttachments(patient, activeVisit, encounter,true);
 	}
 	
 }
