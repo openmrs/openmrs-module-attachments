27193a21600e97ba7fbf1e8cf5a59f846133e520
dilekamadushan <dileka.14@cse.mrt.ac.lk>
2018-03-23 08:14:36 +0530

    ATT-24-1 Add AttachmentsService to api, modified the code on reviews

--- api/src/test/java/org/openmrs/module/attachments/AttachmentsServiceTest.java
+++ api/src/test/java/org/openmrs/module/attachments/AttachmentsServiceTest.java
@@ -7,34 +7,35 @@ import org.openmrs.api.PatientService;
 import org.openmrs.api.VisitService;
 import org.openmrs.module.attachments.obs.Attachment;
 import org.openmrs.test.Verifies;
+import org.openmrs.web.test.BaseModuleWebContextSensitiveTest;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 
-public class AttachmentsServiceTest {
+public class AttachmentsServiceTest extends BaseModuleWebContextSensitiveTest {
 	
 	@Autowired
 	private AttachmentsService as;
 	
 	@Autowired
-	private PatientService ps;
+	private PatientService patientService;
 	
 	@Autowired
-	private VisitService vs;
+	private VisitService visitService;
 	
 	@Autowired
-	private EncounterService es;
+	private EncounterService encounterService;
 	
 	@Test
 	@Verifies(value = "the parameters should return the corresponding attachments", method = "getAttachments(patient, visit, encounter, includeRetired)")
 	public void getAttachments_shouldReturnAttachments() {
 		
 		// Setup
-		Patient patient = ps.getPatient(1);
-		Visit visit = vs.getActiveVisitsByPatient(patient).get(0);
-		Encounter encounter = es.getEncountersByPatient(patient).get(0);
+		Patient patient = patientService.getPatient(0);
+		Visit visit = visitService.getVisit(1);
+		Encounter encounter = encounterService.getEncountersByPatient(patient).get(0);
 		
 		//Replay
 		List<Attachment> attachmentList = as.getAttachments(patient, visit, encounter, true);
