c17ae6aeda2a5bc3e7132334668a3ce0bfbd4673
dilekamadushan <dileka.14@cse.mrt.ac.lk>
2018-04-01 17:04:37 +0530

    ATT-24-1 modified the logic to get conceptComplexList

--- api-2.0/src/test/java/org/openmrs/module/attachments/AttachmentsServiceTest.java
+++ api-2.0/src/test/java/org/openmrs/module/attachments/AttachmentsServiceTest.java
@@ -5,7 +5,6 @@ import org.junit.Test;
 import org.openmrs.Encounter;
 import org.openmrs.Obs;
 import org.openmrs.Patient;
-import org.openmrs.Visit;
 import org.openmrs.api.ConceptService;
 import org.openmrs.api.EncounterService;
 import org.openmrs.api.ObsService;
@@ -17,7 +16,6 @@ import org.openmrs.test.BaseModuleContextSensitiveTest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 
-import java.util.Date;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -50,18 +48,17 @@ public class AttachmentsServiceTest extends BaseModuleContextSensitiveTest {
 	private ObsService os;
 	
 	@Test
-	public void getAttachments_shouldReturnAttachments() throws Exception {
+	public void getAttachments_shouldReturnEncounterAttachments() throws Exception {
 		
 		// Setup
 		List<Obs> complexObsList = testHelper.saveComplexObs(3);
 		
 		Obs obs = complexObsList.get(0);
 		Patient patient = obs.getEncounter().getPatient();
-		Visit visit = obs.getEncounter().getVisit();
 		Encounter encounter = obs.getEncounter();
 		
 		// Replay
-		List<Attachment> actualAttachments = as.getAttachments(patient, visit, encounter, true);
+		List<Attachment> actualAttachments = as.getAttachments(patient, null, encounter, true);
 		
 		// Verify
 		List<Attachment> expectedAttachments = complexObsList.stream().map(Attachment::new).collect(Collectors.toList());
@@ -69,43 +66,5 @@ public class AttachmentsServiceTest extends BaseModuleContextSensitiveTest {
 		Assert.assertArrayEquals(
 		    expectedAttachments.stream().map(Attachment::getUuid).collect(Collectors.toList()).toArray(),
 		    actualAttachments.stream().map(Attachment::getUuid).collect(Collectors.toList()).toArray());
-	}
-	
-	@Test
-	public void getAttachments_shouldFailForNonComplexObs() throws Exception {
-		// Setup
-		List<Obs> complexObsList = testHelper.saveComplexObs(3);
-		
-		Obs obs = complexObsList.get(0);
-		Patient patient = obs.getEncounter().getPatient();
-		Visit visit = obs.getEncounter().getVisit();
-		Encounter encounter = obs.getEncounter();
-		
-		// saving some other obs during the same encounter
-		Obs otherObs = new Obs();
-		otherObs.setConcept(cs.getConcept(3));
-		otherObs.setObsDatetime(new Date());
-		otherObs.setEncounter(encounter);
-		otherObs.setPerson(patient);
-		otherObs.setValueText("Some text value for a test obs.");
-		otherObs = os.saveObs(otherObs, null);
-		Assert.assertNotNull(otherObs.getId());
-		
-		complexObsList.add(otherObs);
-		
-		// Replay
-		List<Attachment> actualAttachments = as.getAttachments(patient, visit, encounter, true);
-		
-		// Verify
-		List<Attachment> expectedAttachments = complexObsList.stream().map(Attachment::new).collect(Collectors.toList());
-		
-		try {
-			Assert.assertArrayEquals(
-			    expectedAttachments.stream().map(Attachment::getUuid).collect(Collectors.toList()).toArray(),
-			    actualAttachments.stream().map(Attachment::getUuid).collect(Collectors.toList()).toArray());
-		}
-		catch (AssertionError e) {
-			e.printStackTrace();
-		}
 	}
 }
--- api/src/main/java/org/openmrs/module/attachments/AttachmentsServiceImpl.java
+++ api/src/main/java/org/openmrs/module/attachments/AttachmentsServiceImpl.java
@@ -1,19 +1,31 @@
 package org.openmrs.module.attachments;
 
+import org.openmrs.Concept;
 import org.openmrs.Encounter;
 import org.openmrs.Obs;
 import org.openmrs.Patient;
 import org.openmrs.Person;
 import org.openmrs.Visit;
 
+import org.openmrs.api.ConceptService;
 import org.openmrs.api.context.Context;
 
 import org.openmrs.module.attachments.obs.Attachment;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class AttachmentsServiceImpl implements AttachmentsService {
+	
+	@Autowired
+	@Qualifier(AttachmentsConstants.COMPONENT_ATT_CONTEXT)
+	protected AttachmentsContext context;
+	
+	@Autowired
+	@Qualifier("conceptService")
+	private ConceptService cs;
 	
 	@Override
 	public List<Attachment> getAttachments(Patient patient, Visit visit, Encounter encounter, boolean includeRetired) {
@@ -22,9 +34,15 @@ public class AttachmentsServiceImpl implements AttachmentsService {
 		List<Attachment> attachmentsList = new ArrayList<>();
 		personList.add(patient);
 		
+		List<String> conceptComplexList = context.getConceptComplexList();
+		List<Concept> questionConcepts = new ArrayList<>();
+		for (String uuid : conceptComplexList) {
+			questionConcepts.add(cs.getConceptByUuid(uuid));
+		}
+		
 		if (visit == null && encounter == null) {
-			List<Obs> obs = Context.getObsService().getObservations(personList, null, null, null, null, null, null, null,
-			    null, null, null, includeRetired);
+			List<Obs> obs = Context.getObsService().getObservations(personList, null, questionConcepts, null, null, null,
+			    null, null, null, null, null, includeRetired);
 			for (Obs observation : obs) {
 				if (observation.isComplex()) {
 					attachmentsList.add(new Attachment(observation));
@@ -35,8 +53,8 @@ public class AttachmentsServiceImpl implements AttachmentsService {
 			List<Encounter> encounterList = new ArrayList<>();
 			encounterList.add(encounter);
 			
-			List<Obs> obs = Context.getObsService().getObservations(personList, encounterList, null, null, null, null, null,
-			    null, null, null, null, includeRetired);
+			List<Obs> obs = Context.getObsService().getObservations(personList, encounterList, questionConcepts, null, null,
+			    null, null, null, null, null, null, includeRetired);
 			for (Obs observation : obs) {
 				if (observation.isComplex()) {
 					attachmentsList.add(new Attachment(observation));
@@ -46,8 +64,8 @@ public class AttachmentsServiceImpl implements AttachmentsService {
 		} else {
 			List<Encounter> encounterList = new ArrayList<>();
 			encounterList.add(encounter);
-			List<Obs> obs = Context.getObsService().getObservations(personList, encounterList, null, null, null, null, null,
-			    null, null, null, null, includeRetired);
+			List<Obs> obs = Context.getObsService().getObservations(personList, encounterList, questionConcepts, null, null,
+			    null, null, null, null, null, null, includeRetired);
 			for (Obs observation : obs) {
 				if (observation.getEncounter().getVisit() == visit && observation.isComplex()) {
 					attachmentsList.add(new Attachment(observation));
