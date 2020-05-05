263e0ca473e86390ebca82a56cf57adb69750fe1
mks-d <dimitri@mekomsolutions.com>
2020-05-05 10:49:48 +0200

    ATT-38: ComplexViewHelper2_0 to log a warning when unsupported view.

--- api-2.0/src/main/java/org/openmrs/module/attachments/obs/ComplexViewHelper2_0.java
+++ api-2.0/src/main/java/org/openmrs/module/attachments/obs/ComplexViewHelper2_0.java
@@ -1,5 +1,9 @@
 package org.openmrs.module.attachments.obs;
 
+import static org.openmrs.obs.ComplexObsHandler.RAW_VIEW;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.openmrs.Obs;
 import org.openmrs.annotation.OpenmrsProfile;
 import org.openmrs.api.ObsService;
@@ -12,17 +16,24 @@ import org.springframework.stereotype.Component;
 @OpenmrsProfile(openmrsPlatformVersion = "2.0.0")
 public class ComplexViewHelper2_0 implements ComplexViewHelper {
 	
+	private final Log log = LogFactory.getLog(getClass());
+	
 	@Autowired
 	private ObsService obsService;
 	
 	@Override
 	public String getView(Obs obs, String view) {
 		
-		if (obsService.getHandler(obs).supportsView(view)) {
+		ComplexObsHandler handler = obsService.getHandler(obs);
+		
+		if (handler.supportsView(view)) {
 			return view;
+		} else {
+			log.warn("The requested view '" + view + "' is not supported by the complex obs handler '"
+			        + handler.getClass().getCanonicalName() + "', reverting to the default view '" + RAW_VIEW + "'.");
 		}
 		
-		return ComplexObsHandler.RAW_VIEW;
+		return RAW_VIEW;
 		
 	}
 }
