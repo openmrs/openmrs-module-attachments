6ace15f47b84adbcfc954c93621ba84a713e2f89
Luis Oliveira <luis.oliveira@icrc.org>
2024-04-16 16:19:56 +0100

    Added filename test

--- omod-1.10/src/test/java/org/openmrs/module/attachments/rest/AttachmentResourceTest.java
+++ omod-1.10/src/test/java/org/openmrs/module/attachments/rest/AttachmentResourceTest.java
@@ -1,5 +1,7 @@
 package org.openmrs.module.attachments.rest;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
@@ -10,13 +12,19 @@ import static org.mockito.MockitoAnnotations.initMocks;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.openmrs.Concept;
+import org.openmrs.ConceptComplex;
+import org.openmrs.ConceptDatatype;
 import org.openmrs.Encounter;
+import org.openmrs.Obs;
 import org.openmrs.Patient;
 import org.openmrs.Visit;
+import org.openmrs.api.ObsService;
 import org.openmrs.api.context.Context;
 import org.openmrs.module.attachments.AttachmentsConstants;
 import org.openmrs.module.attachments.AttachmentsContext;
 import org.openmrs.module.attachments.AttachmentsService;
+import org.openmrs.module.attachments.obs.Attachment;
 import org.openmrs.module.attachments.obs.ComplexDataHelper1_10;
 import org.powermock.api.mockito.PowerMockito;
 import org.powermock.core.classloader.annotations.PrepareForTest;
@@ -35,7 +43,32 @@ public class AttachmentResourceTest {
 		when(Context.getRegisteredComponent(AttachmentsConstants.COMPONENT_ATT_CONTEXT, AttachmentsContext.class))
 		        .thenReturn(ctx);
 	}
-	
+
+	@Test
+	public void get_shouldReturnFilenameProperty() {
+		// Arrange
+		AttachmentResource1_10 res = new AttachmentResource1_10();
+		ObsService service = mock(ObsService.class);
+		PowerMockito.when(Context.getObsService()).thenReturn(service);
+		Obs attachmentObs = new Obs();
+		attachmentObs.setUuid("1234");
+		attachmentObs.setId(1);
+		Concept attachmentConcept = new ConceptComplex();
+		ConceptDatatype datatype = new ConceptDatatype();
+		datatype.setHl7Abbreviation("ED");
+		attachmentConcept.setDatatype(datatype);
+		attachmentObs.setConcept(attachmentConcept);
+		attachmentObs.setValueComplex("m3ks | instructions.default | text/plain | filename.png");
+		when(service.getObsByUuid("1234")).thenReturn(attachmentObs);
+		when(service.getComplexObs(1, AttachmentsConstants.ATT_VIEW_CRUD)).thenReturn(attachmentObs);
+
+		// Act
+		Attachment attachment = res.getByUniqueId("1234");
+
+		// Assert
+		assertThat(attachment.getFilename(), equalTo("filename.png"));
+	}
+
 	@Test
 	public void search_shouldInvokeApiForEncounterAttachments() {
 		// Setup
